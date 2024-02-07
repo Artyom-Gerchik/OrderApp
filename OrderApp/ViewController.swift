7ab21a6988370c22484ab5062a011fa1445dbc34
Artyom-Gerchik <bombruchelyator@gmail.com>
2024-02-08 01:57:08 +0300

    initial commit

---
+++ OrderApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OrderApp
+//
+//  Created by Artyom on 7.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Views
+    
+    
+    
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
